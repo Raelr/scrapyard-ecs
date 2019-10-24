b183661429edea0a1fb2797e57622e98aeee65e4
Aryeh <aryehzinn@hotmail.com>
2019-10-24 19:35:17 +1100

    Added General comments and new tests

--- src/index_allocator.rs
+++ src/index_allocator.rs
@@ -1,5 +1,36 @@
+/// # Scrapyard Index Allocator
+///
+/// The index allocator is the data structure responsible for storing and synchronising all generational
+/// indices as used by the ECS system. The allocator contains two vectors, one which stores all indices,
+/// and one containing all free indices. Free indices are indices which have been previously used by
+/// a previous item or data structure beforehand and were de-allocated at a certain point. If an index
+/// exists within the free vector, then it is immediately used for any entities which require an index
+/// allocation.
+
 use self::super::errors::Error;
 
+/// A generational index is a unique index which stores both the entity's storage index in a collection,
+/// and also its specific instance number (or generation). Essentially, when a structure needs to search for
+/// an element in a collection, it must ensure that both a) the indices are the same, and that b) the
+/// generations are the same. In doing so, we always make sure that the index is pointing toward the correct
+/// data.
+///
+/// ## Examples:
+///
+/// When first created, an index list may have no available entries. When an entity is made, it's index
+/// is created. Since it is the first entity in the list, it is assigned an index of 0, and since it is
+/// the first entity using that index, it is assigned a generation of 0. As such, the generational index
+/// created is: Index: 0 Generation: 0.
+///
+/// If the entity is removed or destroyed in the course of the game, the index is deallocated. In this case,
+/// it is moved into the 'free' vector. When a new entity requests an index, the allocator checks the free
+/// vector and sees that index 0 is free. It therefore assigns 0 to the new entity. Since the index has been
+/// used before, its generation is incremented by one. Therefore, the new entity's index is now:
+/// Index: 0 Generation: 1.
+///
+/// When the new entity now wants to search up anything within its index, it simply checks that both the
+/// index and generation are the same. As such even though the first entity may have elements in a component
+/// list, if its generation does not match, then it is not used.
 #[derive(Clone)]
 pub struct GenerationalIndex {
     pub index: usize,
@@ -20,8 +51,8 @@ impl IndexAllocator {
 
     pub fn new() -> IndexAllocator {
 
-        let entries : Vec<AllocatorEntry> = Vec::with_capacity(1000);
-        let free : Vec<usize> = Vec::with_capacity(1000);
+        let entries : Vec<AllocatorEntry> = Vec::with_capacity(1024);
+        let free : Vec<usize> = Vec::with_capacity(1024);
 
          IndexAllocator {
             entries,
@@ -30,27 +61,15 @@ impl IndexAllocator {
     }
 
     pub fn allocate(&mut self) -> Result<GenerationalIndex, Error> {
-
-        let mut index : usize = 0;
-        let mut generation : u32 = 0;
-
         if !self.free.is_empty() {
-
             let free_idx = self.free[0];
             let mut entry  = &mut self.entries[free_idx];
-
             entry.generation += 1;
             entry.live = true;
-
-            index = self.free.pop()?;
-            generation = entry.generation;
-
+            return Ok(GenerationalIndex {index: self.free.pop()?, generation: entry.generation});
         } else {
-
             self.entries.push(AllocatorEntry { live: true, generation: 0});
-            index = self.entries.len() - 1;
+            Ok(GenerationalIndex {index: self.entries.len() - 1, generation: 0})
         }
-
-        Ok(GenerationalIndex {index, generation})
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,17 +9,35 @@ mod tests {
 
     #[test]
     fn generate_index() -> Result<(), Error> {
-
+        println!("\nTest one:");
         let mut allocator = IndexAllocator::new();
-
         let index = allocator.allocate()?;
-
         let success = index.index == 0 && index.generation == 0;
-
-        println!("{}, {}", index.index, index.generation);
-
+        println!("Index: {}, {}", index.index, index.generation);
         assert_eq!(success, true);
-
         Ok(())
+    }
+
+    #[test]
+    fn generate_multiple_indices() -> Result<(), Error> {
+        let mut allocator = IndexAllocator::new();
+        let first = allocator.allocate()?;
+        let second = allocator.allocate()?;
+        let third = allocator.allocate()?;
+
+        println!("\nTest two:");
+        println!("First: {}, {}", first.index, first.generation);
+        println!("Second: {}, {}", second.index, second.generation);
+        println!("Third: {}, {}", third.index, third.generation);
+
+        let success = first.index == 0 && first.generation == 0
+            && second.index == 1 && second.generation == 0
+            && third.index == 2 && third.generation == 0;
+        assert_eq!(success, true);
+        Ok(())
+    }
+
+    fn deallocate_indices() -> Result<(), Error> {
+
     }
 }
